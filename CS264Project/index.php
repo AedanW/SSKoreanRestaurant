dcbbd93e0fa4426d664fa3a3e3535759293db5e9
RandyYunjaeLee <randy_lee98@hotmail.com>
2019-03-27 13:36:45 -0700

    Update index.php

--- CS264Project/index.php
+++ CS264Project/index.php
@@ -20,7 +20,7 @@ li a {
   text-align: center;
   padding: 14px 20px;
   text-decoration: none;
-  border-radius:12px 12px 0px 0px;
+  border-radius:12px 12px 12px 12px;
 }
 
 li a:hover:not(.active) {
@@ -34,20 +34,17 @@ li a:hover:not(.active) {
 </head>
 <body>
 <ul>
-  <li><a class="active" href="#home" >Home</a></li>
-  <li><a href="https://www.tsn.ca" target="myframe">Sports</a></li>
+  <li><a class="active" href="#home" >SS Korean Restaurant</a></li>
+  <li><a href="Menu.html" target="myframe">Menu</a></li>
   <li><a href="contact.html" target="myframe">Contact</a></li>
   <li><a href="about.html" target="myframe">About</a></li>
-  <li><a href="userlogin.html" target="_blank">Member Login</li>
+  <li><a href="userlogin.html" target="_blank">Log in and order now</li>
 </ul>
 <br style="clear:left">
 
-<div style="background-color:lightyellow; width:50%; height:650px; float:left">
-<h2>Welcome to my home page!!</h2>
-</div>
 
-<div style="background-color:lightblue; width:50%; height:650px; float:left">
-<iframe name="myframe" src="https://www.castanet.net" width=100% height=650 ></iframe>
+<div style="background-color:lightblue; width:100%; height:650px; float:left">
+    <iframe name="myframe" src="start.html" width=100% height=650 ></iframe>
 </div>
 <br style="clear:left">
 
